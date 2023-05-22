e7982d4575d8bb019c2fae1b0c5b1e4233ecd21b
Hannah Ek <ekhannahek@gmail.com>
2023-05-22 09:53:38 +0200

    7 unit tests finished

--- src/6-filter-numbers.js
+++ src/6-filter-numbers.js
@@ -1,3 +1,13 @@
-export const filterNumbers = (array, largerThan) => {
-  return array
-}
+export const isEmpty = (stringArrayOrObject) => {
+  const type = typeof stringArrayOrObject;
+
+  if (type === 'string') {
+    return stringArrayOrObject === ''
+  } else if (Array.isArray(stringArrayOrObject)) {
+    return stringArrayOrObject.length === 0
+  } else if (type === 'object') {
+    return Object.keys(stringArrayOrObject).length === 0
+  };
+
+  return false;
+} 
